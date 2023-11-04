5caa39153e57a7aeabec58e56753c142c6b53dbb
Danil <danil0316@mail.ru>
2023-11-05 00:33:29 +0500

    add "User.cs" model

---
+++ PokeAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PokeAPI.Models
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        [Column("user_id")]
+        public Guid UserId { get; private set; } = Guid.NewGuid();
+
+        [Column("username")]
+        public string Username { get; set; }
+
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
