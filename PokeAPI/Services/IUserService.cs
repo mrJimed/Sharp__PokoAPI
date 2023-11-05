fd9dc37ad6ef6c26062fc8856a64d521a64ba2a4
Danil <danil0316@mail.ru>
2023-11-05 18:15:41 +0500

    add UserService

---
+++ PokeAPI/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using PokeAPI.Models;
+
+namespace PokeAPI.Services
+{
+    public interface IUserService
+    {
+        Task<User?> GetUser(string username, string password);
+        Task<User?> GetUserInfo(string username);
+    }
+}
---
+++ PokeAPI/Services/UsersService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PokeAPI.Models;
+using PokeAPI.Services.DbContexts;
+
+namespace PokeAPI.Services
+{
+    public class UsersService : IUserService
+    {
+        private PokeDbContext pokeDb;
+
+        public UsersService([FromServices] PokeDbContext pokeDb)
+        {
+            this.pokeDb = pokeDb;
+        }
+
+        public async Task<User?> GetUserInfo(string username)
+        {
+            var user = await pokeDb.Users.FirstOrDefaultAsync(u => u.Username.Equals(username));
+            return user;
+        }
+
+        public async Task<User?> GetUser(string username, string password)
+        {
+            var user = await pokeDb.Users.FirstOrDefaultAsync(u => u.Username.Equals(username) && u.Password.Equals(password));
+            return user;
+        }
+    }
+}
